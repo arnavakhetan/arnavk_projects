450d8061fd2ca2f53ef50f948ff3e499e23ab587
arnavakhetan <83293565+arnavakhetan@users.noreply.github.com>
2024-03-29 12:06:19 -0700

    Add files via upload

---
+++ PythonProjects/lab1.py.py
@@ -0,0 +1,21 @@
+int_1 = int(input("Enter an integer: "))
+int_2 = int(input("Enter another integer: "))
+
+sum = int(input(f"\nWhat is {int_1} + {int_2}? "))
+print("Your answer: ", sum)
+print("Correct answer: ", int(int_1 + int_2))
+
+difference = int(input(f"\nWhat is {int_1} - {int_2}? "))
+print("Your answer: ", difference)
+print("Correct answer: ", int(int_1 - int_2))
+
+quotient = float(input(f"\nWhat is {int_1} / {int_2}? "))
+print("Your answer: ", quotient)
+print("Correct answer: ", float(int_1 / int_2))
+
+product = int(input(f"\nWhat is {int_1} * {int_2}? "))
+print("Your answer: ", product)
+print("Correct answer: ", int(int_1 * int_2))
+
+print("\nThanks for playing!")
+
