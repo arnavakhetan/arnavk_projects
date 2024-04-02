e0133eb39ed01ec78b1225ff7f6fd13940678289
arnavakhetan <83293565+arnavakhetan@users.noreply.github.com>
2024-04-01 23:50:47 -0700

    Update FactorialCalculator.py

--- PythonProjects/FactorialCalculator.py
+++ PythonProjects/FactorialCalculator.py
@@ -1,14 +1,25 @@
-while True:
-  n = int(input("Enter a positive integer: "))
-  
-factorial = 1
-statement = f"{n}! ="
+invalid_integer = True
+while invalid_integer:
+  try:
+    n = int(input("Enter a positive integer: "))
+    if n >= 0:
+      invalid_integer = False
+    else:
+      print("The number is not positive. Please enter a positive integer.\n")
+  except ValueError:
+    print("That's not an integer. Please enter a positive integer.\n")
 
-for i in range(1, n + 1):
-  factorial = factorial * i
-  if i < n:
-    statement = statement + f" {i} x"
-  else:
-    statement = statement + f" {i} = {factorial}"
+if n == 0:
+  print("0! = 1")
+else:
+  factorial = 1
+  statement = f"{n}! ="
 
-print(statement)
+  for i in range(1, n + 1):
+    factorial = factorial * i
+    if i < n:
+      statement = statement + f" {i} x"
+    else:
+      statement = statement + f" {i} = {factorial}"
+
+  print(statement)
