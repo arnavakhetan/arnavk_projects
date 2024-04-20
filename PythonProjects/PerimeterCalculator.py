d83751f46c5686438ad44a35e1fbb609def29885
arnavakhetan <83293565+arnavakhetan@users.noreply.github.com>
2024-04-19 22:33:44 -0700

    Update PerimeterCalculator.py

--- PythonProjects/PerimeterCalculator.py
+++ PythonProjects/PerimeterCalculator.py
@@ -1,8 +1,3 @@
-# Arnav Khetan
-# Section A
-# Problem 1A: Circle Perimeter
-
-
 import math   
 
 def distance(x1, y1, x2, y2):    
@@ -13,7 +8,6 @@ def perim(r):
     peri = 2 * math.pi * r
     return peri
 
-
 def main(): 
     x1 = float(input("Enter x for center point: "))
     y1 = float(input("Enter y for center point: "))
@@ -22,11 +16,5 @@ def main():
     r = distance(x1, y1, x2, y2)
     perimeter = perim(r)
     print(f"Permimeter of circle centered at ({x1}, {y1}) with radius {r} is {perimeter}")
-    
-	# These values represent two points on a Cartesian plane (x1, y1) and (x2, y2)
-	# Calls the distance(x1, y1, x2, y2) function to calculate the distance between two points.
-	# Calls the perim(r) function to calculate the perimeter of a circle with radius r. 
-	# Prints the circle’s center point, radius, and perimeter.
-	# Example of print statement: Perimeter of circle centered at (0.0 , 0.0) with radius 11.180339887498949 is 70.24814731040726
 
 main()
