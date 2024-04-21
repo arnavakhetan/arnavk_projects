540ab4907fb2e3c4aa1d7934596d7e596acf403f
arnavakhetan <83293565+arnavakhetan@users.noreply.github.com>
2024-04-20 21:54:13 -0700

    Add files via upload

---
+++ PythonProjects/arnav_pentagon.py
@@ -0,0 +1,20 @@
+# Arnav Khetan
+# Section B
+# Problem 1B: Spiral Pentagon
+
+import turtle 
+
+
+def draw_spiral(my_turtle):
+	length = 2
+	for i in range(1, 101):
+		my_turtle.forward(length)
+		my_turtle.right(72)
+		length = length + 2
+def main():
+	screen = turtle.Screen()
+	my_turtle = turtle.Turtle()
+	draw_spiral(my_turtle)
+	screen.exitonclick()
+
+main()
