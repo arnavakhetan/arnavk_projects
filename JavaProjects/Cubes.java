d8bd0789caba149da88fc89f3d01617b855770e8
Arnav Khetan <83293565+arnavakhetan@users.noreply.github.com>
2024-09-11 22:12:32 -0700

    Update Cubes.java

--- JavaProjects/Cubes.java
+++ JavaProjects/Cubes.java
@@ -4,13 +4,13 @@ class Cubes {
     public static void main(String[] args) {
         int num = 1;
         int cube = 1;
-        while (cube < 2000) { // While Loop with the check for cube being less than 2000
-            cube = num * num * num; // Simple cube formula
+        while (cube < 2000) { 
+            cube = num * num * num; 
             if (cube >= 2000) {
                 break;
             }
-            System.out.println(cube); // Prints out the cubed value
-            num++; // Increments 1 each time to the value of "num"
+            System.out.println(cube); 
+            num++; 
         }
     }
 }
