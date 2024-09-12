d14af1f3f461d871b8d1e510559d7d923630ec12
Arnav Khetan <83293565+arnavakhetan@users.noreply.github.com>
2024-09-11 22:10:19 -0700

    Update Triangle.java

--- JavaProjects/Triangle.java
+++ JavaProjects/Triangle.java
@@ -2,16 +2,16 @@
 // The last number on each line is the first number of that line multiplied by the "number of numbers" in that line.
 class Triangle {
     public static void main(String[] args) {
-        for (int num = 1; num <= 15; num++) { // First for loop is for each individual line
-            for (int mult = 1; mult <= num; mult++) { // Second for loop is responsible for the multiplication
-                int result = num * mult; // and for the printing out of numbers on each line themselves
+        for (int num = 1; num <= 15; num++) { 
+            for (int mult = 1; mult <= num; mult++) {
+                int result = num * mult; 
                 System.out.print(result);
-                if (mult == num) { // This if statement is there to avoid an extra space at the end of each line
-                    continue; // Otherwise (without lines 9 to 11), the correct output still comes but there will be an extra space at the end of each line
+                if (mult == num) { 
+                    continue; 
                 }
                 System.out.print(" ");
             }
-            System.out.println(); // Goes to the next line
+            System.out.println(); 
         }
     }
 }
