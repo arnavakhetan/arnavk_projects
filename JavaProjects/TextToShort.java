a32897b597305bdf23e8b817ed19f14679b8f0ae
Arnav Khetan <83293565+arnavakhetan@users.noreply.github.com>
2024-09-24 21:01:06 -0700

    Update TextToShort.java

--- JavaProjects/TextToShort.java
+++ JavaProjects/TextToShort.java
@@ -3,17 +3,15 @@
 // Trying to give a number that is equivalent to a number not in the range of a short value will make it wrap around the underflow/overflow.
 class TextToShort {
     public static void main(String[] args) {
-        // I am doing the version without any commas/spaces in the input
-        if (args.length == 0 || args[0].length() == 0 || args[0].length() > 16) { // No command line argument or 0 characters or more than 16 characters
+        if (args.length == 0 || args[0].length() == 0 || args[0].length() > 16) { 
             System.err.println("Error: Incorrect input provided");
-            return; // Exits the main method if error found
+            return; 
         }
         String inputValue = args[0];
         short convertedValue = 0;
         for (int i = 0; i < inputValue.length(); i++) {
-            char bit = inputValue.charAt(i); // Takes each '0' and '1' value separately for conversion
-            // charAt() Taken from: https://stackoverflow.com/questions/22861407/search-for-particular-character-inside-a-string
-            convertedValue = (short) (convertedValue << 1); // Shift the current short value left by 1 (bitwise operator)
+            char bit = inputValue.charAt(i); 
+            convertedValue = (short) (convertedValue << 1); 
             if (bit == '1') {
                 convertedValue = (short) (convertedValue | 1);
             }
